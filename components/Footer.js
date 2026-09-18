4094f9db0d877affba363f5ebc60cb2a4ffe9908
N. A. Gavkani <245768563+avannargess@users.noreply.github.com>
2026-09-17 22:22:29 -0400

    changed footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,6 @@ export default function Footer() {
   return (
     <footer className="site-footer">
       <div className="container">
-        <p>© {new Date().getFullYear()} N. A. Gavkani — <em>Dragon & Daffodil</em></p>
         <p className="small">
           © {new Date().getFullYear()} N. A. Gavkani All Rights Reserved {" "}
          
